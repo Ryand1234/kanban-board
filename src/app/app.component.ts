18f6d48f142ce50665fd75f8a6b8eda61de911eb
Riyan Dhiman <riyandhiman14@gmail.com>
2021-07-09 23:10:38 +0530

    status change now supported

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,41 +8,49 @@ export class AppComponent implements OnInit{
   title = 'Kanban Board';
   tasks = [
     {
+      id: 1,
       title: 'Do HomeWork',
       status: 'pending',
       description: 'A new Task'
     },
     {
+      id: 2,
       title: 'Call someone',
       status: 'inprogress',
       description: 'A new Task'
     },
     {
+      id: 3,
       title: 'Call someone',
       status: 'inprogress',
       description: 'A new Task'
     },
     {
+      id: 4,
       title: 'Call someone',
       status: 'pending',
       description: 'A new Task'
     },
     {
+      id: 5,
       title: 'Call someone',
       status: 'completed',
       description: 'A new Task'
     },
     {
+      id: 6,
       title: 'Call someone',
       status: 'inprogress',
       description: 'A new Task'
     },
     {
+      id: 7,
       title: 'Call someone',
       status: 'completed',
       description: 'A new Task'
     }
   ]
+    curTask: any = {}
     inProgressTasks: any
     completeTasks: any
     pendingTasks: any
@@ -51,5 +59,27 @@ export class AppComponent implements OnInit{
      this.inProgressTasks  = this.tasks.filter( (e) => e.status === 'inprogress')
      this.completeTasks = this.tasks.filter( (e) => e.status === 'completed')
      this.pendingTasks = this.tasks.filter( (e) => e.status === 'pending')
+    }
+
+    statusChanged({id, status, newStatus}) {
+      this.curTask = {}
+      if (status === 'pending') {
+        this.curTask = this.pendingTasks.filter(e => e.id == id)
+        this.pendingTasks = this.pendingTasks.filter(e => e.id != id)
+      } else if (status === 'inprogress') {
+        this.curTask = this.inProgressTasks.filter(e => e.id == id)
+        this.inProgressTasks = this.inProgressTasks.filter(e => e.id != id)
+      } else {
+        this.curTask = this.completeTasks.filter(e => e.id == id)
+        this.completeTasks = this.completeTasks.filter(e => e.id != id)
+      }
+      this.curTask = this.curTask[0]
+      this.curTask.status = newStatus
+      switch(newStatus) {
+        case 'pending': this.pendingTasks.push(this.curTask); break;
+        case 'inprogress': this.inProgressTasks.push(this.curTask); break;
+        case 'completed': this.completeTasks.push(this.curTask); break;
+      }
+
     }
 }
--- src/app/status-card/status-card.component.ts
+++ src/app/status-card/status-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-status-card',
@@ -9,18 +9,30 @@ export class StatusCardComponent implements OnInit {
   // status: string = ""
   @Input() status: String;
   @Input() title: String;
+  @Input() taskId: Number;
   @Input() description: String;
+  @Output() changedStatus: EventEmitter<any> = new EventEmitter<any>();
   statusClass: String = 'pending'
   constructor() { }
   isActive: Boolean = false
+  emittedObject: Object = {}
   ngOnInit(): void {
-    console.log(this.description)
+    console.log(this.taskId)
     this.statusClass = this.status;
     
   }
   
   change() {
     this.isActive = !this.isActive
+  }
+
+  changeStatus(id, status, newStatus) {
+    this.emittedObject = {
+      id,
+      status,
+      newStatus
+    }
+    this.changedStatus.emit(this.emittedObject)
   }
 
 }

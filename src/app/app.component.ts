137d9ca3abf4c88fa78c7121c20cee64ee1ceac3
Riyan Dhiman <riyandhiman14@gmail.com>
2021-07-09 19:09:11 +0530

    imporved UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit{
   title = 'Kanban Board';
+  tasks = [
+    {
+      title: 'Do HomeWork',
+      status: 'pending'
+    },
+    {
+      title: 'Call someone',
+      status: 'inprogress'
+    },
+    {
+      title: 'Call someone',
+      status: 'inprogress'
+    },
+    {
+      title: 'Call someone',
+      status: 'pending'
+    },
+    {
+      title: 'Call someone',
+      status: 'completed'
+    },
+    {
+      title: 'Call someone',
+      status: 'inprogress'
+    },
+    {
+      title: 'Call someone',
+      status: 'completed'
+    }
+  ]
+    inProgressTasks: any
+    completeTasks: any
+    pendingTasks: any
+    // console.log('C: ', this.pendingTasks)
     ngOnInit() {
-
+     this.inProgressTasks  = this.tasks.filter( (e) => e.status === 'inprogress')
+     this.completeTasks = this.tasks.filter( (e) => e.status === 'completed')
+     this.pendingTasks = this.tasks.filter( (e) => e.status === 'pending')
     }
 }
--- src/app/status-card/status-card.component.ts
+++ src/app/status-card/status-card.component.ts
@@ -1,13 +1,15 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-status-card',
   templateUrl: './status-card.component.html',
   styleUrls: ['./status-card.component.css']
 })
-export class StatusCardComponent implements OnInit {
+export class StatusCardComponent implements OnInit, AfterViewInit {
   // status: string = ""
+  @ViewChild('.dropdown') dropdown: ElementRef;
   @Input() status: String;
+  @Input() title: String;
   statusClass: String = 'pending'
   constructor() { }
 
@@ -16,4 +18,13 @@ export class StatusCardComponent implements OnInit {
     this.statusClass = this.status;
   }
 
+  ngAfterViewInit() {
+    console.log(this.dropdown)
+  }
+
+  // dropdown: any = document.getElementById('.dropdown')
+  // this.dropdown.addEventListener('click', (e) => {
+  //   e.stopPropagation();
+  //   this.dropdown.classList.toggle('is-active');
+  // })
 }
